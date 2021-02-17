a5887b8e71dfa287449334e7da5ad9b72510d149
Shane Koppers <shanekoppers@hotmail.com>
2021-02-17 13:44:09 +0100

    Add apple signin auth

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,21 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
+import AppleSignin from 'react-apple-signin-auth';
+
+/** Apple Signin button */
+const MyAppleSigninButton = () => (
+  <AppleSignin
+    authOptions={{
+      clientId: 'com.example.web',
+      scope: 'email name',
+      redirectURI: 'https://example.com',
+      usePopup: true
+    }}
+    render={(props) => <button {...props}>My Custom Button</button>}
+  />
+);
+
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -8,58 +23,7 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <MyAppleSigninButton />
     </div>
   )
 }
